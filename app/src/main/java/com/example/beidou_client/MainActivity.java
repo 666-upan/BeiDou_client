4af3449f49d2ae05d4c4ce7ffa694bd3da68e075
1234 <1319796378@qq.com>
2019-07-28 19:25:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/beidou_client/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.beidou_client;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    public LocationClient mLocationClient = null;
+    private MyLocationListener myListener = new MyLocationListener();
+    //BDAbstractLocationListener为7.2版本新增的Abstract类型的监听接口
+//原有BDLocationListener接口暂时同步保留。具体介绍请参考后文第四步的说明
+    public void onCreate() {
+        mLocationClient = new LocationClient(getApplicationContext());
+        //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);
+        //注册监听函数
+    }
+
+    LocationClientOption option = new LocationClientOption();
+
+option.setLocationMode(LocationMode.Hight_Accuracy);
+//可选，设置定位模式，默认高精度
+//LocationMode.Hight_Accuracy：高精度；
+//LocationMode. Battery_Saving：低功耗；
+//LocationMode. Device_Sensors：仅使用设备；
+
+option.setCoorType("bd09ll");
+//可选，设置返回经纬度坐标类型，默认GCJ02
+//GCJ02：国测局坐标；
+//BD09ll：百度经纬度坐标；
+//BD09：百度墨卡托坐标；
+//海外地区定位，无需设置坐标类型，统一返回WGS84类型坐标
+
+option.setScanSpan(1000);
+//可选，设置发起定位请求的间隔，int类型，单位ms
+//如果设置为0，则代表单次定位，即仅定位一次，默认为0
+//如果设置非0，需设置1000ms以上才有效
+
+option.setOpenGps(true);
+//可选，设置是否使用gps，默认false
+//使用高精度和仅用设备两种定位模式的，参数必须设置为true
+
+option.setLocationNotify(true);
+//可选，设置是否当GPS有效时按照1S/1次频率输出GPS结果，默认false
+
+option.setIgnoreKillProcess(false);
+//可选，定位SDK内部是一个service，并放到了独立进程。
+//设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
+
+option.SetIgnoreCacheException(false);
+//可选，设置是否收集Crash信息，默认收集，即参数为false
+
+option.setWifiCacheTimeOut(5*60*1000);
+//可选，V7.2版本新增能力
+//如果设置了该接口，首次启动定位时，会先判断当前Wi-Fi是否超出有效期，若超出有效期，会先重新扫描Wi-Fi，然后定位
+
+option.setEnableSimulateGps(false);
+//可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
+
+mLocationClient.setLocOption(option);
+//mLocationClient为第二步初始化过的LocationClient对象
+//需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+//更多LocationClientOption的配置，请参照类参考中LocationClientOption类的详细说明
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
